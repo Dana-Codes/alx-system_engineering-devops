d54de8e0a1bbbadf79a0c5ef5982d6c488a701c5
CodingbyDaniel <109753547+Dana-Codes@users.noreply.github.com>
2024-01-20 15:12:26 +0100

    okay

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Read: http://puppet-lint.com/checks/
 # Using Puppet, install puppet-lint.
-package { 'puppet-lint':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip',
 }
